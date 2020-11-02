8195a2408a0b78bf4b57ecd2ae1e925800bd2e71
vasic2000 <vasic2000@yandex.ru>
2020-11-03 00:27:48 +0300

    Звуки

--- app/src/main/java/ru/vasic2000/hitcovid/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/hitcovid/MainActivity.kt
@@ -1,14 +1,20 @@
 package ru.vasic2000.hitcovid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.System.exit
 import java.util.*
 import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
     var score : Int = 0
@@ -16,6 +22,24 @@ class MainActivity : AppCompatActivity() {
     var handler : Handler = Handler()
     var runnable : Runnable = Runnable {}
 
+    private var backPressedTime: Long = 0
+    private lateinit var backToast: Toast
+
+    lateinit var sounds : SoundPool
+    var kheh : Int = 0
+    var aaa : Int = 0
+    var hit1 : Int = 0
+    var hit2 : Int = 0
+    var hit3 : Int = 0
+    var hit4 : Int = 0
+    var hit5 : Int = 0
+    var hit6 : Int = 0
+    var hit7 : Int = 0
+    var hit8 : Int = 0
+    var hit9 : Int = 0
+
+    val rndSound = Random()
+    var covidKillSound : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +60,10 @@ class MainActivity : AppCompatActivity() {
         exitButton.visibility = View.INVISIBLE
         scoreText.visibility = View.INVISIBLE
         timeText.visibility = View.INVISIBLE
+        Thread.sleep(1000)
     }
 
-    fun game(view : View) {
+    fun game(view: View) {
         startButton.visibility = View.INVISIBLE
         replayButton.visibility = View.INVISIBLE
         exitButton.visibility = View.INVISIBLE
@@ -76,6 +101,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun initialize() {
         score = 0
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            sounds = SoundPool.Builder()
+                .setMaxStreams(2)
+                .build()
+        } else {
+            sounds = SoundPool(2, AudioManager.STREAM_MUSIC, 1)
+        }
+
+        aaa = sounds.load(applicationContext, R.raw.aaa, 1)
+
+        hit1 = sounds.load(applicationContext, R.raw.ay, 1)
+        hit2 = sounds.load(applicationContext, R.raw.oi3, 1)
+        hit3 = sounds.load(applicationContext, R.raw.hu, 1)
+        hit4 = sounds.load(applicationContext, R.raw.ao, 1)
+        hit5 = sounds.load(applicationContext, R.raw.aaa, 1)
+        hit6 = sounds.load(applicationContext, R.raw.otr, 1)
+        hit7 = sounds.load(applicationContext, R.raw.ao, 1)
+        hit8 = sounds.load(applicationContext, R.raw.puk, 1)
+        hit9 = sounds.load(applicationContext, R.raw.o_o, 1)
+
         imageArray = arrayListOf(
             imageView1, imageView2, imageView3, imageView4,
             imageView5, imageView6, imageView7, imageView8,
@@ -90,15 +136,42 @@ class MainActivity : AppCompatActivity() {
         var covidImageName : Int
 
         when (covidPicIndex) {
-            1 -> covidImageName = R.drawable.covid1
-            2 -> covidImageName = R.drawable.covid2
-            3 -> covidImageName = R.drawable.covid3
-            4 -> covidImageName = R.drawable.covid4
-            5 -> covidImageName = R.drawable.covid5
-            6 -> covidImageName = R.drawable.covid6
-            7 -> covidImageName = R.drawable.covid7
-            8 -> covidImageName = R.drawable.covid8
-            else -> covidImageName = R.drawable.covid9
+            1 -> {
+                covidImageName = R.drawable.covid1
+                covidKillSound = hit1
+            }
+            2 -> {
+                covidImageName = R.drawable.covid2
+                covidKillSound = hit2
+            }
+            3 -> {
+                covidImageName = R.drawable.covid3
+                covidKillSound = hit3
+            }
+            4 -> {
+                covidImageName = R.drawable.covid4
+                covidKillSound = hit4
+            }
+            5 -> {
+                covidImageName = R.drawable.covid5
+                covidKillSound = hit5
+            }
+            6 -> {
+                covidImageName = R.drawable.covid6
+                covidKillSound = hit6
+            }
+            7 -> {
+                covidImageName = R.drawable.covid7
+                covidKillSound = hit7
+            }
+            8 -> {
+                covidImageName = R.drawable.covid8
+                covidKillSound = hit8
+            }
+            else -> {
+                covidImageName = R.drawable.covid9
+                covidKillSound = hit9
+            }
         }
 
         for(i in 0 until imageArray.size) {
@@ -125,5 +198,22 @@ class MainActivity : AppCompatActivity() {
     fun increaseScore(view: View) {
         score++
         scoreText.text = "Score: " + score
+        sounds.play(covidKillSound,1f,1f,0,0,0f)
+    }
+
+    override fun onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel()
+            super.onBackPressed()
+            return
+        } else {
+            backToast = Toast.makeText(this, "Press Back to exit again", Toast.LENGTH_SHORT)
+            backToast.show()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+
+    fun exitMain(view: View) {
+        exit(1)
     }
 }
